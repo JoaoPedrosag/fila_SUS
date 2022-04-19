ca75fe83a246dfc679d5afe686797d00357decff
OaisCloud <joaopedro.saggiorato@oais.cloud>
2022-04-18 21:22:06 -0300

    dando inicio firebase

---
+++ lib/src/configuration/domain/external/queue_firestore_datasource.dart
@@ -0,0 +1,10 @@
+import 'package:fila_antendimento/src/configuration/infra/repositories/datasource/queue_datasource.dart';
+
+class QueueFirestoreDatasource implements IQueueDatasource {
+  @override
+  Stream<List<Map>> GetAllQueues() {
+    // TODO: implement GetAllQueues
+    throw UnimplementedError();
+  }
+
+}
